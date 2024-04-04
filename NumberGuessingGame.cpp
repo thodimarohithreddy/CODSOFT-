95803c8341ac2d59d26a9704207079605004435d
thodimarohithreddy <143797739+thodimarohithreddy@users.noreply.github.com>
2024-04-04 18:24:33 +0530

    Add files via upload

---
+++ NumberGuessingGame.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+using namespace std;
+int main() 
+{
+    srand(time(0));
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    while (true) {
+        cout << "Enter your guess (between 1 and 100): ";
+        cin >> guess;
+        if (cin.fail()) {
+            cout << "Invalid input. Please enter a valid number." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+        if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the correct number." << endl;
+            break; 
+        }
+    }
+    return 0;
+}
